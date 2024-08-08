c55fac2f18341a4bc110abb9d0cfb79ef44e264a
IReallyLikeYourPants <michelekryston@gmail.com>
2024-08-08 20:46:52 +0200

    Update authorities.sh

--- sh_files/authorities.sh
+++ sh_files/authorities.sh
@@ -5,7 +5,7 @@ run_commands() {
     gnome-terminal -- bash -c "\
         docker exec -it martsia_ethereum_container bash -c ' \
             sleep 0.3 && \
-            cd MARTSIA-demo/sh_files && \
+            cd MARTSIA-Demo-KoB/sh_files && \
             sleep 0.3 && \
             sh old/authority.sh --authority $authority; \
 	    sleep 10 && \
@@ -18,4 +18,3 @@ count=$(grep -c '^[^#]*''NAME="AUTH' "$filename")
 for ((i=1; i<=$count; i++)); do
     run_commands $i
 done
-
